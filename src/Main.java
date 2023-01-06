579e3ba9f901b1ae2c36adbc883c64fb180f7410
Anna <annadubin@yandex.ru>
2023-01-06 17:27:03 +0300

    Modification

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,14 @@ public class Main {
         int addCount = 1500;
         int bonus = 0;
 
-        {
-                if (addCount > 1000)
-                bonus = addCount / 100;
-            }
-            {
-                if (addCount > 0) ;
-                else System.out.println("Пополнение счета не может быть отрицательным");
-            }
-            count = count + bonus + addCount;
-            System.out.println("На вашем счете: " + count + "рублей." + "Ваш бонус: " + bonus + "рублей");
+        if (addCount > 1000) {
+            bonus = addCount / 100;
+
+            if (addCount > 0) ;
+        } else {
+            System.out.println("Пополнение счета не может быть отрицательным");
         }
+        count = count + bonus + addCount;
+        System.out.println("На вашем счете: " + count + "рублей." + "Ваш бонус: " + bonus + "рублей");
+    }
 }
