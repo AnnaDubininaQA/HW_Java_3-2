09acadae3f512da25d2ca15e654443935a3589cf
Anna <annadubin@yandex.ru>
2023-01-07 20:38:18 +0300

    Modification_2

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,8 @@ public class Main {
         int count = 100;
         int addCount = 1500;
         int bonus = 0;
-
         if (addCount > 1000) {
             bonus = addCount / 100;
-
         }
         int result = count + bonus + addCount;
         System.out.println("Итоговый счет: " + result + " рублей. " + " Бонус: " + bonus + " рублей ");
